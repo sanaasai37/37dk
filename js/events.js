2c30764389b73e1cc5f3f2e6da923bf0bb68052d
sanaasai37 <tori.matsuzaka.ms@gmail.com>
2026-03-08 23:19:55 +0900

    Add files via upload

--- js/events.js
+++ js/events.js
@@ -1,6 +1,21 @@
 const events = {
-  "2026-03-08": [
-    { text: "23:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
+  "2026-03-11": [
+    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
+  ],
+  "2026-03-14": [
+    { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
+  ],
+  "2026-03-18": [
+    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
+  ],
+  "2026-03-21": [
+    { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
+  ],
+  "2026-03-25": [
+    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
+  ],
+  "2026-03-28": [
+    { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
   ],
   // "2026-03-07": [
   //   { text: "0:00　37DK公開", link: "http://37dk.jp" },
