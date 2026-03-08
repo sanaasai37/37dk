791db01cc3a9da5177319eb8b8c3cb822f6ad3b2
sanaasai37 <tori.matsuzaka.ms@gmail.com>
2026-03-09 00:25:22 +0900

    Add files via upload

--- js/memories.js
+++ js/memories.js
@@ -1,10 +1,17 @@
 const memories = [
     {
+        date: "2026-03-09",
+        title: "37DK",
+        image: "memory-img/img_9023.jpg",
+        link: "https://ameblo.jp/sana-asai/entry-12959123362.html"
+    },
+    {
         date: "2026-03-07",
         title: "37DK公開",
-        image: "../rooms/memory-img/floorplan.png",
+        image: "memory-img/floorplan.png",
         link: "http://37dk.jp"
     },
+
     // {
     //     date: "2026.06.18",
     //     title: "YouTube投稿",
