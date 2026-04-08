b9faede89eaf292bf6aa031b39abd0a02efeaedc
sanaasai37 <tori.matsuzaka.ms@gmail.com>
2026-04-08 23:48:23 +0900

    Add files via upload

--- js/events.js
+++ js/events.js
@@ -1,6 +1,6 @@
 const events = {
-  "2026-04-08": [
-    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
+  "2026-04-09": [
+    { text: "21:00　TikTok Live", link: "https://www.tiktok.com/@_sana_asai/" }
   ],
   "2026-04-11": [
     { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
@@ -15,6 +15,36 @@ const events = {
     { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
   ],
   "2026-04-25": [
+    { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
+  ],
+  "2026-04-29": [
+    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
+  ],
+  "2026-05-02": [
+    { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
+  ],
+  "2026-05-06": [
+    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
+  ],
+  "2026-05-09": [
+    { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
+  ],
+  "2026-05-13": [
+    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
+  ],
+  "2026-05-16": [
+    { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
+  ],
+  "2026-05-20": [
+    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
+  ],
+  "2026-05-23": [
+    { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
+  ],
+  "2026-05-27": [
+    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
+  ],
+  "2026-05-30": [
     { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
   ],
   // "2026-03-07": [
