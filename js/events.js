ba74c44d227ff0767bf6f1dffe5b688bfaf5d366
sanaasai37 <tori.matsuzaka.ms@gmail.com>
2026-04-16 18:17:53 +0900

    Add files via upload

--- js/events.js
+++ js/events.js
@@ -1,13 +1,4 @@
 const events = {
-  "2026-04-09": [
-    { text: "21:00　TikTok Live", link: "https://www.tiktok.com/@_sana_asai/" }
-  ],
-  "2026-04-11": [
-    { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
-  ],
-  "2026-04-15": [
-    { text: "21:00　さなろぐ更新", link: "https://www.youtube.com/@sanalog3.7" }
-  ],
   "2026-04-18": [
     { text: "21:00　さなちゅぶ。更新", link: "https://www.youtube.com/@_sana_asai" }
   ],
